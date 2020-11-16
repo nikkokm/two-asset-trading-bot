30737129d5fc0aa08626b75db6bfa0386aa23a24
Nikolas <nikolaskjeldsen@gmail.com>
2020-11-16 13:17:44 +0100

    Change from PDP to MTUM  in order to allow short sales

--- main.py
+++ main.py
@@ -37,28 +37,28 @@ def trade(prediction, weight):
 
 
 
-    if prediction == 1:  # we buy PDP if we predicted a 1
+    if prediction == 1:  # we buy MTUM if we predicted a 1
 
-        pdp_quote = api.get_last_quote('PDP')
-        qty_to_buy = int(cash_to_spend/float(pdp_quote.askprice))
+        mtum_quote = api.get_last_quote('MTUM')
+        qty_to_buy = int(cash_to_spend/float(MTUM_quote.askprice))
 
         vtv_quote = api.get_last_quote('VTV')
         qty_to_sell = int(cash_to_spend/float(vtv_quote.bidprice))
 
-        # this block makes sure we go long in PDP
-        try:  # we check if we already have a PDP position:
-            pos = api.get_position(symbol='PDP')
+        # this block makes sure we go long in MTUM
+        try:  # we check if we already have a MTUM position:
+            pos = api.get_position(symbol='MTUM')
             current_qty = int(pos.qty)  # our existing position
 
             if pos.side == 'short':  # if we are short, we close the short and go long
                 current_qty = current_qty * (-1)
-                api.submit_order(symbol='PDP', qty=current_qty+qty_to_buy, side='buy', type='market', time_in_force='day')
+                api.submit_order(symbol='MTUM', qty=current_qty+qty_to_buy, side='buy', type='market', time_in_force='day')
 
             else:  # if we are already long, we do nothing OR we scale up/down our position according to "weight" (?)
                 pass
 
-        except:  # if we get an error, we do not have PDP and will therefore go long
-            api.submit_order(symbol='PDP', qty=qty_to_buy, side='buy', type='market', time_in_force='day')
+        except:  # if we get an error, we do not have MTUM and will therefore go long
+            api.submit_order(symbol='MTUM', qty=qty_to_buy, side='buy', type='market', time_in_force='day')
 
         # this block makes sure we short VTV
         try:
@@ -79,8 +79,8 @@ def trade(prediction, weight):
 
     elif prediction == 0:  # we buy VTV if we predicted a 0
 
-        pdp_quote = api.get_last_quote('PDP')
-        qty_to_sell = int(cash_to_spend / float(pdp_quote.bidprice))
+        mtum_quote = api.get_last_quote('MTUM')
+        qty_to_sell = int(cash_to_spend / float(mtum_quote.bidprice))
 
         vtv_quote = api.get_last_quote('VTV')
         qty_to_buy = int(cash_to_spend / float(vtv_quote.askprice))
@@ -100,19 +100,19 @@ def trade(prediction, weight):
         except:  # we do not have a VTV position so we go long
             api.submit_order(symbol='VTV', qty=qty_to_buy, side='buy', type='market', time_in_force='day')
 
-        # this block makes sure we are short PDP
+        # this block makes sure we are short MTUM
         try:
-            pos = api.get_position(symbol='PDP')
+            pos = api.get_position(symbol='MTUM')
             current_qty = int(pos.qty)
 
             if pos.side == 'long':  # if we are already long PDP, we close it and go short
-                api.submit_order(symbol='PDP', qty=current_qty+qty_to_sell, side='sell', type='market', time_in_force='day')
+                api.submit_order(symbol='MTUM', qty=current_qty+qty_to_sell, side='sell', type='market', time_in_force='day')
 
             else:  # if we are already short, we do nothing or we re-scale the position
                 pass
 
-        except:  # if we do not have a PDP position, we go short
-            api.submit_order(symbol='PDP', qty=qty_to_sell, side='sell', type='market', time_in_force='day')
+        except:  # if we do not have a MTUM position, we go short
+            api.submit_order(symbol='MTUM', qty=qty_to_sell, side='sell', type='market', time_in_force='day')
 
         return
 
