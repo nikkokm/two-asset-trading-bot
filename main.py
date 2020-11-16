62c568fc41180ae054439ca55fb92a1aaa9ec793
Nikolas <nikolaskjeldsen@gmail.com>
2020-11-16 15:57:38 +0100

    Algo Scheduling

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ import datetime
 import time
 
 
-
 def get_trade():
     """
     This function gets the predicted trade for today and the relative capital amount to trade
@@ -33,17 +32,15 @@ def trade(prediction, weight):
     """
     account = api.get_account()
     cash = float(account.cash)
-    cash_to_spend = cash*weight
-
-
+    cash_to_spend = cash * weight
 
     if prediction == 1:  # we buy MTUM if we predicted a 1
 
         mtum_quote = api.get_last_quote('MTUM')
-        qty_to_buy = int(cash_to_spend/float(MTUM_quote.askprice))
+        qty_to_buy = int(cash_to_spend / float(mtum_quote.askprice))
 
         vtv_quote = api.get_last_quote('VTV')
-        qty_to_sell = int(cash_to_spend/float(vtv_quote.bidprice))
+        qty_to_sell = int(cash_to_spend / float(vtv_quote.bidprice))
 
         # this block makes sure we go long in MTUM
         try:  # we check if we already have a MTUM position:
@@ -52,7 +49,8 @@ def trade(prediction, weight):
 
             if pos.side == 'short':  # if we are short, we close the short and go long
                 current_qty = current_qty * (-1)
-                api.submit_order(symbol='MTUM', qty=current_qty+qty_to_buy, side='buy', type='market', time_in_force='day')
+                api.submit_order(symbol='MTUM', qty=current_qty + qty_to_buy, side='buy', type='market',
+                                 time_in_force='day')
 
             else:  # if we are already long, we do nothing OR we scale up/down our position according to "weight" (?)
                 pass
@@ -66,7 +64,8 @@ def trade(prediction, weight):
             current_qty = int(pos.qty)
 
             if pos.side == 'long':  # if we are long VTV we close it and go short
-                api.submit_order(symbol='VTV', qty=current_qty+qty_to_sell, side='sell', type='market', time_in_force='day')
+                api.submit_order(symbol='VTV', qty=current_qty + qty_to_sell, side='sell', type='market',
+                                 time_in_force='day')
 
             else:  # if we are already short, we do nothing OR re-scale our position
                 pass
@@ -91,8 +90,9 @@ def trade(prediction, weight):
             current_qty = int(pos.qty)
 
             if pos.side == 'short':  # if we are short, close it and go long
-                current_qty = current_qty*(-1)
-                api.submit_order(symbol='VTV', qty=current_qty+qty_to_buy, side='buy', type='market', time_in_force='day')
+                current_qty = current_qty * (-1)
+                api.submit_order(symbol='VTV', qty=current_qty + qty_to_buy, side='buy', type='market',
+                                 time_in_force='day')
 
             else:  # we are already long, so we do nothing or we re-scale
                 pass
@@ -106,7 +106,8 @@ def trade(prediction, weight):
             current_qty = int(pos.qty)
 
             if pos.side == 'long':  # if we are already long PDP, we close it and go short
-                api.submit_order(symbol='MTUM', qty=current_qty+qty_to_sell, side='sell', type='market', time_in_force='day')
+                api.submit_order(symbol='MTUM', qty=current_qty + qty_to_sell, side='sell', type='market',
+                                 time_in_force='day')
 
             else:  # if we are already short, we do nothing or we re-scale the position
                 pass
@@ -142,7 +143,7 @@ def is_market_open():
     """
 
     nyc = timezone('America/New_York')
-    today_us_time =  datetime.datetime.today().astimezone(nyc)
+    today_us_time = datetime.datetime.today().astimezone(nyc)
     today_us_str = today_us_time.strftime('%Y-%m-%d')
     calendar = api.get_calendar(start=today_us_str, end=today_us_str)[0]
     open_date = calendar.date.strftime('%Y-%m-%d')
@@ -153,50 +154,36 @@ def is_market_open():
         return False
 
 
-
 api = tradeapi.REST(ALPACA_KEY_ID, ALPACA_SECRET_KEY, ALPACA_ENDPOINT)
 
-# This block of code is to for running the algo non-stop
-# while True:
-#     trading_day_today = is_market_open()
-#
-#     if trading_day_today is False:
-#         time.sleep(1800)  # wait 30 minutes
-#         continue  # go back and check if now, 30 minutes later, we are in a trading day (from the POV of NYC)
-#
-#     else:
-#         traded_today = check_trade_history()
-#
-#         if traded_today is True:
-#             time.sleep(1800)  # wait 30 minutes
-#             continue  # go back to top of loop
-#
-#         else:
-#
-#             clock = api.get_clock()
-#             if clock.is_open is False:  # Today is a trading day, we did not trade yet and the market is closed.
-#                 time.sleep(30)  # wait only 30 seconds if we have not traded yet
-#                 continue
-#
-#             else:  # Today is a trading day, we did not trade yet, the market is open.
-#                 data_aggregator.update_data()  # update the CSV with most recent data
-#                 df = pd.read_csv('Data/database.csv', index_col='Date')
-#                 df = train_model.compute_label(df=df)
-#                 pred, weight = get_trade()
-#                 trade(prediction=pred, weight=weight)
-#                 time.sleep(7200)  # wait 20 hours before resuming while loop
-#                 continue
+#This block of code is to for running the algo non-stop
+while True:
+    trading_day_today = is_market_open()
 
+    if trading_day_today is False:
+        time.sleep(1800)  # wait 30 minutes
+        continue  # go back and check if now, 30 minutes later, we are in a trading day (from the POV of NYC)
 
-data_aggregator.update_data()
-df = pd.read_csv('Data/database.csv', index_col='Date')
-df = train_model.compute_label(df=df)
-train_model.train_model_in_sample(df)  # just running this to see that all the code works
-pred, weight = get_trade()
-trade(pred, weight)
+    else:
+        traded_today = check_trade_history()
 
+        if traded_today is True:
+            time.sleep(1800)  # wait 30 minutes
+            continue  # go back to top of loop
 
+        else:
 
+            clock = api.get_clock()
+            if clock.is_open is False:  # Today is a trading day, we did not trade yet and the market is closed.
+                time.sleep(30)  # wait only 30 seconds if we have not traded yet
+                continue
 
-
-
+            else:  # Today is a trading day, we did not trade yet, the market is open.
+                data_aggregator.update_data()  # update the CSV with most recent data
+                df = pd.read_csv('Data/database.csv', index_col='Date')
+                df = train_model.compute_label(df=df)
+                pred, weight = get_trade()
+                trade(prediction=pred, weight=weight)
+                print('Trade Executed...')
+                time.sleep(7200)  # wait 20 hours before resuming while loop
+                continue
